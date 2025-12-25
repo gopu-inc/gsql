8c89d7f25e6c1f9f0e87830c7d3eedc3431f8b80
Mauricio-100 <ceoseshell@gmail.com>
2025-12-25 02:33:17 +0100

    Update __init__.py

--- gsql/__init__.py
+++ gsql/__init__.py
@@ -14,7 +14,7 @@ from typing import Optional, Dict, Any, Union, List
 
 # ==================== VERSION & METADATA ====================
 
-__version__ = "3.0.9"
+__version__ = "3.1.0"
 __author__ = "Gopu Inc."
 __license__ = "MIT"
 __copyright__ = "Copyright 2024 Gopu Inc."
